42387c1194a24c1096d6a7658bca2b9513fd2eb8
meshakeugenee <meshakeugene@gmail.com>
2025-10-24 10:30:26 +0530

    Update TicketController.java

--- demo/src/main/java/com/example/demo/controllers/TicketController.java
+++ demo/src/main/java/com/example/demo/controllers/TicketController.java
@@ -33,4 +33,4 @@ public class TicketController {
        ticketService.deleteTicket(id);
        return ResponseEntity.noContent().build();
    }
-}//where all the methods returns all the JSON response
+}//where all the methods returns all the JSON response  and handles http request get,put,.....
